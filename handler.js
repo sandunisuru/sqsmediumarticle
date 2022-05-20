05feefcea3b18da05368946e5804df96b90bee72
Sandun Isuru Niraj <SandunN@99x.lk>
2022-05-20 18:44:05 +0530

    Initial push

---
+++ handler.js
@@ -0,0 +1,37 @@
+'use strict';
+const aws = require('aws-sdk');
+const sqs = new aws.SQS({ apiVersion: '2012-11-05', region: 'us-east-1' });
+const sqsQueueUrl = process.env.QUEUE_URL;
+
+module.exports.messageProducer = async (event) => {
+
+  for (var i = 0; i < 10; i++) {
+    await sqs.sendMessage({
+      MessageBody: 'success',
+      QueueUrl: sqsQueueUrl
+    }).promise();
+  }
+
+  for (var i = 0; i < 10; i++) {
+    await sqs.sendMessage({
+      MessageBody: 'failed',
+      QueueUrl: sqsQueueUrl
+    }).promise();
+  }
+
+  return true;
+};
+
+module.exports.messageConsumer = async (event) => {
+
+  var sqsRecords = event.Records;
+  var batchItemFailures = [];
+
+  if (sqsRecords.length > 0) {
+    for await (const message of sqsRecords) {
+      if (message.body === 'failed')
+        batchItemFailures.push({ itemIdentifier: message.messageId })
+    }
+  }
+  return { batchItemFailures };
+};
